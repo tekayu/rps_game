1cd04e5b2f271eb3935db0cc4b9465cb257403cc
tekayu <joshuaodb@icloud.com>
2025-03-27 16:48:04 +1000

    Added getComputerChoice to script.js to get random result from the computer.
    Added getComputerChoice to script.js to get input
    from user.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+        return 'rock';
+        case 1:
+        return 'paper';
+        case 2:
+        return 'scissors';
+    }
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt('Please choose rock, paper, or scissors.');
+    console.log(humanChoice)
+    return humanChoice;
+}
+
+getHumanChoice();
